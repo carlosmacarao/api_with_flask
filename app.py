446b536f0a3b1de850be2203ae72ac98a37c3a2c
carlosmacarao <carlosmacarao9@gmail.com>
2024-07-30 16:19:16 +0200

    new columns

--- app.py
+++ app.py
@@ -14,6 +14,8 @@ db = SQLAlchemy(app)
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(120), nullable = False)
+    price = db.Column(db.Float, nullable = False)
+    description = db.Column(db.Text, nullable = True)
 
 @app.route('/')
 def hello_world():
